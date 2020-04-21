163bd98dd12eb3367a1945007e499a7dbba9abc1
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-04-21 08:30:57 +0100

    update

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,30 @@
+import qualified Data.Map as M
+import Morse
+import Test.QuickCheck
+
+
+-- Now we set up our generators for ensuring that the random values
+-- QuickCheck uses to test our program are sensible for our Morse code
+-- program
+
+allowedChars :: [Char]
+allowedChars = M.keys letterToMorse
+
+allowedMorse :: [Morse]
+allowedMorse = M.elems letterToMorse
+
+charGen :: Gen Char
+charGen = elements allowedChars
+
+morseGen :: Gen Morse
+morseGen = elements allowedMorse
+
+
+prop_thereAndBackAgain :: Property
+prop_thereAndBackAgain =
+ forAll charGen
+  (\c -> ((charToMorse c)
+   >>= morseToChar) == Just c)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck prop_thereAndBackAgain
